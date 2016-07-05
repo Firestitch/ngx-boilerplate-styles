6e9fc9224ab82da71f1e0596a38ad8abb7f135b0
Raymond <ray@firestitch.com>
2016-07-05 13:46:06 -0400

    commit dist

---
+++ src/dist/js/docs-setup.js
@@ -0,0 +1,187 @@
+NG_DOCS={
+  "sections": {
+    "api": "API Documentation"
+  },
+  "pages": [
+    {
+      "section": "api",
+      "id": "app.alertService",
+      "shortName": "app.alertService",
+      "type": "service",
+      "moduleName": "app",
+      "shortDescription": "alertService",
+      "keywords": "alertservice api app factory service"
+    },
+    {
+      "section": "api",
+      "id": "app.apiService",
+      "shortName": "app.apiService",
+      "type": "service",
+      "moduleName": "app",
+      "shortDescription": "apiService",
+      "keywords": "api apiservice app conifg factory service true urlencoded"
+    },
+    {
+      "section": "api",
+      "id": "app.appService",
+      "shortName": "app.appService",
+      "type": "service",
+      "moduleName": "app",
+      "shortDescription": "appService",
+      "keywords": "api app appservice factory service"
+    },
+    {
+      "section": "api",
+      "id": "app.authService",
+      "shortName": "app.authService",
+      "type": "service",
+      "moduleName": "app",
+      "shortDescription": "authService",
+      "keywords": "access api app authenticate authservice cleans credentials destroying email factory getuser input login logout logs method object ongoing passes password promised redirecting save service session user"
+    },
+    {
+      "section": "api",
+      "id": "app.configService",
+      "shortName": "app.configService",
+      "type": "service",
+      "moduleName": "app",
+      "shortDescription": "configService",
+      "keywords": "api app configservice data existing factory nice promise request service token tokens validates wraps"
+    },
+    {
+      "section": "api",
+      "id": "app.controller:BodyCtrl",
+      "shortName": "BodyCtrl",
+      "type": "function",
+      "moduleName": "app",
+      "shortDescription": "BodyCtrl",
+      "keywords": "api app bodyctrl controller function"
+    },
+    {
+      "section": "api",
+      "id": "app.controller:Trade_categoriesCtrl",
+      "shortName": "Trade_categoriesCtrl",
+      "type": "function",
+      "moduleName": "app",
+      "shortDescription": "Trade_category",
+      "keywords": "api app controller function trade_category"
+    },
+    {
+      "section": "api",
+      "id": "app.directive:alert",
+      "shortName": "alert",
+      "type": "directive",
+      "moduleName": "app",
+      "shortDescription": "alert",
+      "keywords": "alert api app directive"
+    },
+    {
+      "section": "api",
+      "id": "app.directive:colorContainer",
+      "shortName": "colorContainer",
+      "type": "directive",
+      "moduleName": "app",
+      "shortDescription": "alert",
+      "keywords": "alert api app directive"
+    },
+    {
+      "section": "api",
+      "id": "app.directive:compareTo",
+      "shortName": "compareTo",
+      "type": "directive",
+      "moduleName": "app",
+      "shortDescription": "compareTo",
+      "keywords": "api app compareto directive"
+    },
+    {
+      "section": "api",
+      "id": "app.GOOGLEAPIKEY",
+      "shortName": "app.GOOGLEAPIKEY",
+      "type": "service",
+      "moduleName": "app",
+      "shortDescription": "GOOGLEAPIKEY",
+      "keywords": "api app constant googleapikey service"
+    },
+    {
+      "section": "api",
+      "id": "app.historyService",
+      "shortName": "app.historyService",
+      "type": "service",
+      "moduleName": "app",
+      "shortDescription": "historyService",
+      "keywords": "api app helper historyservice service set visited"
+    },
+    {
+      "section": "api",
+      "id": "app.Pollyfills",
+      "shortName": "app.Pollyfills",
+      "type": "service",
+      "moduleName": "app",
+      "shortDescription": "Pollyfills",
+      "keywords": "api app factory pollyfills service"
+    },
+    {
+      "section": "api",
+      "id": "app.sessionService",
+      "shortName": "app.sessionService",
+      "type": "service",
+      "moduleName": "app",
+      "shortDescription": "sessionService",
+      "keywords": "api app factory service sessionservice"
+    },
+    {
+      "section": "api",
+      "id": "app.storeService",
+      "shortName": "app.storeService",
+      "type": "service",
+      "moduleName": "app",
+      "shortDescription": "storeService",
+      "keywords": "access allows api app convert decrypt decrypted decrypts easy encrypt encrypted encrypts factory fetch function input key local method obj object optional passed remove removes requested return returns saved saves service session set specific storage storeservice str string"
+    },
+    {
+      "section": "api",
+      "id": "app.TIMEZONES",
+      "shortName": "app.TIMEZONES",
+      "type": "service",
+      "moduleName": "app",
+      "shortDescription": "TIMEZONES",
+      "keywords": "api app constant service timezones"
+    },
+    {
+      "section": "api",
+      "id": "app.trade_categoryService",
+      "shortName": "app.trade_categoryService",
+      "type": "service",
+      "moduleName": "app",
+      "shortDescription": "trade_categoryService",
+      "keywords": "api app factory service trade_categoryservice"
+    },
+    {
+      "section": "api",
+      "id": "rfx.directive:rAutogrow",
+      "shortName": "rAutogrow",
+      "type": "directive",
+      "moduleName": "rfx",
+      "shortDescription": "Resize textarea automatically to the size of its text content.",
+      "keywords": "api automatically class content directive getcomputedstyle html input-block-level module ng-model pollyfill r-autogrow resize rfx size text textarea window"
+    },
+    {
+      "section": "api",
+      "id": "rfx.directive:rAutogrow",
+      "shortName": "rAutogrow",
+      "type": "directive",
+      "moduleName": "rfx",
+      "shortDescription": "Resize textarea automatically to the size of its text content.",
+      "keywords": "api automatically class content directive don html input-block-level matter module ng-model number r-autogrow resize rfx size text textarea"
+    }
+  ],
+  "apis": {
+    "api": true
+  },
+  "html5Mode": true,
+  "editExample": true,
+  "startPage": "/",
+  "scripts": [
+    "angular.min.js"
+  ]
+};
